e37f81ebec5307fd8d97d7e1023001da48995df0
Mathew <mwakre29@gmail.com>
2021-05-12 17:59:20 -0700

    update database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-adjacent-55986",
-    user: "postgres",
-    password: "test",
-    database: "smart_brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
